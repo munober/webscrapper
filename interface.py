8c70099fb8a39788e55af185a3cb54448b417b68
Teo G. Fratiloiu <teodor.fratiloiu@gmail.com>
2020-04-16 22:31:41 +0200

    wokring dynamic page crawler for puppy pics

--- interface.py
+++ interface.py
@@ -14,7 +14,6 @@ from PyQt5.QtWidgets import QWidget
 
 ERROR_MSG = "ERROR"
 
-# Create a subclass of QMainWindow to setup the calculator's GUI
 class PyCalcUi(QMainWindow):
     """PyCalc's View (GUI)."""
 
@@ -103,7 +102,6 @@ def evaluateExpression(expression):
 
     return result
 
-
 # Create a Controller class to connect the GUI and the model
 class PyCalcCtrl:
     """PyCalc's Controller."""
@@ -138,8 +136,6 @@ class PyCalcCtrl:
         self._view.display.returnPressed.connect(self._calculateResult)
         self._view.buttons["C"].clicked.connect(self._view.clearDisplay)
 
-
-# Client code
 def main():
     """Main function."""
     # Create an instance of `QApplication`
@@ -152,7 +148,6 @@ def main():
     PyCalcCtrl(model=model, view=view)
     # Execute calculator's main loop
     sys.exit(pycalc.exec_())
-
 
 if __name__ == "__main__":
     main()
