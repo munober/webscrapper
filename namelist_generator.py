a0d6f5788fc8d11cf3b8dc25947abdbc515a867d
Teo G. Fratiloiu <teodor.fratiloiu@gmail.com>
2020-04-17 19:07:28 +0200

    functioning namelsit generator, adding clout metric for sorting worthy names apart from 50s actors

--- namelist_generator.py
+++ namelist_generator.py
@@ -1,24 +1,71 @@
-# imdb top 10000 actors/actresses
-# actually results in 9996 names when removing duplicates
+# imdb actors/actresses name generator
+from numpy import size
 from requests import get
 from bs4 import BeautifulSoup
 import math
 
-url = 'https://www.imdb.com/search/name/?gender=male,female&start={page}&ref_=rlm'
-index = 1
+url_topactors = 'https://www.imdb.com/search/name/?gender=male,female&start={page}&ref_=rlm' # top actors
+url_movies_alltime = 'https://www.imdb.com/chart/top' # top 250 alltime great movies
+url_movies_current = 'https://www.imdb.com/chart/moviemeter' # top 100 current popular movies
+url_series_alltime = 'https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250'
+url_series_current = 'https://www.imdb.com/chart/tvmeter/?ref_=nv_tvv_mptv'
+
 actors = []
-while index < 9952:
-    response = get(url.format(page=index))
+
+# Scraping top current actors and directors
+response = get(url_movies_current)
+html_soup = BeautifulSoup(response.text, 'html.parser')
+movie_containers = html_soup.find_all('td', class_ = 'titleColumn') #div class where actor names listed
+print('Scraping currently popular movies')
+for item in movie_containers:
+    actors.append(item.a.attrs.get('title').strip()) # actual html container for name, .strip() removes spaces
+
+# Scraping all time greatest actors and directors
+response = get(url_movies_alltime)
+html_soup = BeautifulSoup(response.text, 'html.parser')
+movie_containers = html_soup.find_all('td', class_ = 'titleColumn') #div class where actor names listed
+print('Scraping all time greatest movies')
+for item in movie_containers:
+    actors.append(item.a.attrs.get('title').strip()) # actual html container for name, .strip() removes spaces
+
+# Scraping top current tv series
+response = get(url_series_current)
+html_soup = BeautifulSoup(response.text, 'html.parser')
+movie_containers = html_soup.find_all('td', class_ = 'titleColumn') #div class where actor names listed
+print('Scraping currently popular tv series')
+for item in movie_containers:
+    actors.append(item.a.attrs.get('title').strip()) # actual html container for name, .strip() removes spaces
+
+# Scraping all time greatest tv series
+response = get(url_series_alltime)
+html_soup = BeautifulSoup(response.text, 'html.parser')
+movie_containers = html_soup.find_all('td', class_ = 'titleColumn') #div class where actor names listed
+print('Scraping all time greatest tv series')
+for item in movie_containers:
+    actors.append(item.a.attrs.get('title').strip()) # actual html container for name, .strip() removes spaces
+
+actors_formatted = []
+for actor in actors:
+    split = []
+    split = actor.split(",")
+    for index in range (0, len(split)):
+        actors_formatted.append(split[index].replace("(dir.)", "").strip())
+
+# Scraping top 3k actors
+index = 1
+print('Scraping top 3k imdb actors')
+while index < 2952:
+    response = get(url_topactors.format(page=index))
     html_soup = BeautifulSoup(response.text, 'html.parser')
     movie_containers = html_soup.find_all('div', class_ = 'lister-item-content') #div class where actor names listed
     for item in movie_containers:
-        actors.append(item.h3.a.text.strip()) # actual html container for name, .strip() removes spaces
-    print('Running on page ', math.floor(index / 50 + 1), '/ 200', end="\r")
+        actors_formatted.append(item.h3.a.text.strip()) # .strip() removes spaces
+    print('Running on page ', math.floor(index / 50 + 1), '/ 60', end="\r")
     index = index + 50 # iterating on to the next pages
 
-actors = list(dict.fromkeys(actors)) #removing duplicates
-actors.sort() #alphabetical sorting
+actors_formatted = list(dict.fromkeys(actors_formatted)) #removing duplicates
+actors_formatted.sort() #alphabetical sorting
 
-with open("imdbtop10k.txt","w+") as output:
-    for actor in actors:
+with open("dataset/imdbactors.txt","w+") as output:
+    for actor in actors_formatted:
         output.write(actor + '\n')
