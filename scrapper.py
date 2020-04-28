98a0a4b63cc8f2ea96d5835ae761ac3504e87ddf
Teo G. Fratiloiu <teodor.fratiloiu@gmail.com>
2020-04-28 19:40:08 +0200

    moved ui object class to main script file to work around import problems

--- scrapper.py
+++ scrapper.py
@@ -4,7 +4,9 @@ import os
 import hashlib
 import sys
 
+from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import *
+
 from selenium import webdriver
 import time, os, requests, io
 from bs4 import BeautifulSoup
@@ -15,7 +17,6 @@ from namelist_generator import generate_list
 from google_link_collector import fetch_image_urls_google
 from faces import check_folder, preprocess_image
 from zipfile import ZipFile
-from layout import Ui_Dialog
 
 # Paths and options
 if os.name == "nt":
@@ -157,7 +158,6 @@ filter_mode = args.filter
 preprocess_mode = args.preprocess
 zip_mode = args.zip
 gui_mode = args.gui
-search_mode = False
 custom_list = args.custom
 target_path_imdb = "./dataset/images_imdb"
 target_path_google = "./dataset/images_google"
@@ -431,6 +431,205 @@ def start_search():
 
 
 # GUI related stuff
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(520, 400)
+        self.bottom_buttons = QDialogButtonBox(Dialog)
+        self.bottom_buttons.setGeometry(QtCore.QRect(20, 360, 481, 32))
+        self.bottom_buttons.setOrientation(QtCore.Qt.Horizontal)
+        self.bottom_buttons.setStandardButtons(QDialogButtonBox.Close|QDialogButtonBox.Help|QDialogButtonBox.RestoreDefaults)
+        self.bottom_buttons.setCenterButtons(False)
+        self.bottom_buttons.setObjectName("bottom_buttons")
+        self.tabWidget = QTabWidget(Dialog)
+        self.tabWidget.setGeometry(QtCore.QRect(20, 10, 481, 261))
+        self.tabWidget.setObjectName("tabWidget")
+        self.manual = QWidget()
+        self.manual.setObjectName("manual")
+        self.manual_search_term = QPlainTextEdit(self.manual)
+        self.manual_search_term.setGeometry(QtCore.QRect(80, 10, 120, 30))
+        self.manual_search_term.setObjectName("manual_search_term")
+        self.label_7 = QLabel(self.manual)
+        self.label_7.setGeometry(QtCore.QRect(10, 10, 71, 31))
+        self.label_7.setObjectName("label_7")
+        self.headless_2 = QCheckBox(self.manual)
+        self.headless_2.setGeometry(QtCore.QRect(20, 160, 131, 17))
+        self.headless_2.setObjectName("headless_2")
+        self.label_9 = QLabel(self.manual)
+        self.label_9.setGeometry(QtCore.QRect(20, 90, 91, 16))
+        self.label_9.setObjectName("label_9")
+        self.google_2 = QRadioButton(self.manual)
+        self.google_2.setGeometry(QtCore.QRect(20, 110, 82, 17))
+        self.google_2.setObjectName("google_2")
+        self.imdb_2 = QRadioButton(self.manual)
+        self.imdb_2.setGeometry(QtCore.QRect(20, 130, 82, 17))
+        self.imdb_2.setObjectName("imdb_2")
+        self.sample_size_manual = QSpinBox(self.manual)
+        self.sample_size_manual.setGeometry(QtCore.QRect(160, 110, 80, 22))
+        self.sample_size_manual.setObjectName("sample_size_manual")
+        self.sample_size_manual.setMaximum(100)
+        self.label_10 = QLabel(self.manual)
+        self.label_10.setGeometry(QtCore.QRect(160, 90, 61, 16))
+        self.label_10.setObjectName("label_10")
+        self.label_11 = QLabel(self.manual)
+        self.label_11.setGeometry(QtCore.QRect(260, 90, 61, 16))
+        self.label_11.setObjectName("label_11")
+        self.label_12 = QLabel(self.manual)
+        self.label_12.setGeometry(QtCore.QRect(360, 90, 61, 16))
+        self.label_12.setObjectName("label_12")
+        self.delay_2 = QSpinBox(self.manual)
+        self.delay_2.setGeometry(QtCore.QRect(360, 110, 42, 22))
+        self.delay_2.setObjectName("delay_2")
+        self.timeout_2 = QSpinBox(self.manual)
+        self.timeout_2.setGeometry(QtCore.QRect(260, 110, 42, 22))
+        self.timeout_2.setObjectName("timeout_2")
+        self.run_manual_search = QPushButton(self.manual)
+        self.run_manual_search.setGeometry(QtCore.QRect(390, 170, 75, 23))
+        self.run_manual_search.setObjectName("run_manual_search")
+        self.stop_manual_search = QPushButton(self.manual)
+        self.stop_manual_search.setGeometry(QtCore.QRect(390, 200, 75, 23))
+        self.stop_manual_search.setObjectName("stop_manual_search")
+        self.tabWidget.addTab(self.manual, "")
+        self.listsearch = QWidget()
+        self.listsearch.setObjectName("listsearch")
+        self.list_len = QSpinBox(self.listsearch)
+        self.list_len.setGeometry(QtCore.QRect(80, 60, 80, 22))
+        self.list_len.setMinimumSize(QtCore.QSize(0, 0))
+        self.list_len.setAutoFillBackground(False)
+        self.list_len.setObjectName("list_len")
+        self.list_len.setMaximum(5000)
+        self.label_8 = QLabel(self.listsearch)
+        self.label_8.setGeometry(QtCore.QRect(20, 60, 61, 21))
+        self.label_8.setObjectName("label_8")
+        self.pushButton = QPushButton(self.listsearch)
+        self.pushButton.setGeometry(QtCore.QRect(20, 20, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_4 = QPushButton(self.listsearch)
+        self.pushButton_4.setGeometry(QtCore.QRect(250, 20, 75, 23))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.headless = QCheckBox(self.listsearch)
+        self.headless.setGeometry(QtCore.QRect(20, 160, 131, 17))
+        self.headless.setObjectName("headless")
+        self.label = QLabel(self.listsearch)
+        self.label.setGeometry(QtCore.QRect(20, 90, 91, 16))
+        self.label.setObjectName("label")
+        self.google = QRadioButton(self.listsearch)
+        self.google.setGeometry(QtCore.QRect(20, 110, 82, 17))
+        self.google.setObjectName("google")
+        self.imdb = QRadioButton(self.listsearch)
+        self.imdb.setGeometry(QtCore.QRect(20, 130, 82, 17))
+        self.imdb.setObjectName("imdb")
+        self.label_2 = QLabel(self.listsearch)
+        self.label_2.setGeometry(QtCore.QRect(160, 90, 61, 16))
+        self.label_2.setObjectName("label_2")
+        self.sample_size_list = QSpinBox(self.listsearch)
+        self.sample_size_list.setGeometry(QtCore.QRect(160, 110, 80, 22))
+        self.sample_size_list.setObjectName("sample_size_list")
+        self.sample_size_list.setMaximum(100)
+        self.timeout = QSpinBox(self.listsearch)
+        self.timeout.setGeometry(QtCore.QRect(260, 110, 42, 22))
+        self.timeout.setObjectName("timeout")
+        self.label_3 = QLabel(self.listsearch)
+        self.label_3.setGeometry(QtCore.QRect(260, 90, 61, 16))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QLabel(self.listsearch)
+        self.label_4.setGeometry(QtCore.QRect(360, 90, 61, 16))
+        self.label_4.setObjectName("label_4")
+        self.delay = QSpinBox(self.listsearch)
+        self.delay.setGeometry(QtCore.QRect(360, 110, 42, 22))
+        self.delay.setObjectName("delay")
+        self.run_list_search = QPushButton(self.listsearch)
+        self.run_list_search.setGeometry(QtCore.QRect(390, 170, 75, 23))
+        self.run_list_search.setObjectName("run_list_search")
+        self.stop_list_search = QPushButton(self.listsearch)
+        self.stop_list_search.setGeometry(QtCore.QRect(390, 200, 75, 23))
+        self.stop_list_search.setObjectName("stop_list_search")
+        self.tabWidget.addTab(self.listsearch, "")
+        self.filter = QWidget()
+        self.filter.setObjectName("filter")
+        self.run_filter = QPushButton(self.filter)
+        self.run_filter.setGeometry(QtCore.QRect(390, 170, 75, 23))
+        self.run_filter.setObjectName("run_filter")
+        self.run_filter.clicked.connect(run_filter_mode)
+        self.stop_filter = QPushButton(self.filter)
+        self.stop_filter.setGeometry(QtCore.QRect(390, 200, 75, 23))
+        self.stop_filter.setObjectName("stop_filter")
+        self.tabWidget.addTab(self.filter, "")
+        self.preprocesses = QWidget()
+        self.preprocesses.setObjectName("preprocesses")
+        self.height = QSpinBox(self.preprocesses)
+        self.height.setGeometry(QtCore.QRect(110, 30, 80, 22))
+        self.height.setObjectName("height")
+        self.height.setMaximum(1000)
+        self.width = QSpinBox(self.preprocesses)
+        self.width.setGeometry(QtCore.QRect(10, 30, 80, 22))
+        self.width.setObjectName("width")
+        self.width.setMaximum(1000)
+        self.label_5 = QLabel(self.preprocesses)
+        self.label_5.setGeometry(QtCore.QRect(10, 10, 61, 16))
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QLabel(self.preprocesses)
+        self.label_6.setGeometry(QtCore.QRect(110, 10, 61, 16))
+        self.label_6.setObjectName("label_6")
+        self.grayscale = QCheckBox(self.preprocesses)
+        self.grayscale.setGeometry(QtCore.QRect(10, 80, 131, 17))
+        self.grayscale.setObjectName("grayscale")
+        self.zip = QCheckBox(self.preprocesses)
+        self.zip.setGeometry(QtCore.QRect(10, 100, 161, 17))
+        self.zip.setObjectName("zip")
+        self.run_prep = QPushButton(self.preprocesses)
+        self.run_prep.setGeometry(QtCore.QRect(390, 170, 75, 23))
+        self.run_prep.setObjectName("run_prep")
+        self.stop_preproc = QPushButton(self.preprocesses)
+        self.stop_preproc.setGeometry(QtCore.QRect(390, 200, 75, 23))
+        self.stop_preproc.setObjectName("stop_preproc")
+        self.tabWidget.addTab(self.preprocesses, "")
+
+        self.retranslateUi(Dialog)
+        self.tabWidget.setCurrentIndex(0)
+        self.bottom_buttons.accepted.connect(Dialog.accept)
+        self.bottom_buttons.rejected.connect(Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "WebScrapper 1.1"))
+        self.manual_search_term.setPlainText(_translate("Dialog", "Leonardo DiCaprio"))
+        self.label_7.setText(_translate("Dialog", "Search Term"))
+        self.headless_2.setText(_translate("Dialog", "Run in headless mode"))
+        self.label_9.setText(_translate("Dialog", "Search platform"))
+        self.google_2.setText(_translate("Dialog", "Google"))
+        self.imdb_2.setText(_translate("Dialog", "IMDb"))
+        self.label_10.setText(_translate("Dialog", "Sample size"))
+        self.label_11.setText(_translate("Dialog", "Timeout"))
+        self.label_12.setText(_translate("Dialog", "Delay"))
+        self.run_manual_search.setText(_translate("Dialog", "Run"))
+        self.stop_manual_search.setText(_translate("Dialog", "Stop"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.manual), _translate("Dialog", "Manual"))
+        self.label_8.setText(_translate("Dialog", "List length"))
+        self.pushButton.setText(_translate("Dialog", "Generate list"))
+        self.pushButton_4.setText(_translate("Dialog", "Custom list"))
+        self.headless.setText(_translate("Dialog", "Run in headless mode"))
+        self.label.setText(_translate("Dialog", "Search platform"))
+        self.google.setText(_translate("Dialog", "Google"))
+        self.imdb.setText(_translate("Dialog", "IMDb"))
+        self.label_2.setText(_translate("Dialog", "Sample size"))
+        self.label_3.setText(_translate("Dialog", "Timeout"))
+        self.label_4.setText(_translate("Dialog", "Delay"))
+        self.run_list_search.setText(_translate("Dialog", "Run"))
+        self.stop_list_search.setText(_translate("Dialog", "Stop"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.listsearch), _translate("Dialog", "Search from list"))
+        self.run_filter.setText(_translate("Dialog", "Run"))
+        self.stop_filter.setText(_translate("Dialog", "Stop"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.filter), _translate("Dialog", "Filter"))
+        self.label_5.setText(_translate("Dialog", "Width"))
+        self.label_6.setText(_translate("Dialog", "Height"))
+        self.grayscale.setText(_translate("Dialog", "Grayscale"))
+        self.zip.setText(_translate("Dialog", "Generate zipfile when done"))
+        self.run_prep.setText(_translate("Dialog", "Run"))
+        self.stop_preproc.setText(_translate("Dialog", "Stop"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.preprocesses), _translate("Dialog", "Preprocesses"))
+
 
 def run_gui():
     app = QApplication([])
@@ -439,22 +638,15 @@ def run_gui():
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec_())
-    ui.run_filter.clicked.connect(run_filter_mode())
-
-if (not filter_mode) and (not preprocess_mode) and (not zip_mode) and (not gui_mode):
-    search_mode = True
-
-if filter_mode:
-    run_filter_mode()
-elif preprocess_mode:
-    run_preprocesses()
-elif zip_mode:
-    run_zip()
 
 if gui_mode:
-    search_mode = False
     run_gui()
-
-# Running search mode
-if search_mode:
-    start_search()
+else:
+    if filter_mode:
+        run_filter_mode()
+    if preprocess_mode:
+        run_preprocesses()
+    if zip_mode:
+        run_zip()
+    if (not filter_mode) and (not preprocess_mode) and (not zip_mode):
+        start_search()
